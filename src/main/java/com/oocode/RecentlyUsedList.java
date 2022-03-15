260784110e58313a32a4e49df11e3692dbd8f9a1
WST14 <p@a.com>
2022-03-15 11:13:57 +0000

    Item order, add items.

--- src/main/java/com/oocode/RecentlyUsedList.java
+++ src/main/java/com/oocode/RecentlyUsedList.java
@@ -1,14 +1,17 @@
 package com.oocode;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class RecentlyUsedList {
-	private final List<String> RecentlyUsedList = new ArrayList<>();
+	private final List<String> recentlyUsedList = new ArrayList<>();
 
-	public Collection<String> answer() {
-		return RecentlyUsedList;
+	public List<String> getRecentlyUsedList() {
+		return this.recentlyUsedList;
+	}
+
+	public void add(final String input) {
+		this.recentlyUsedList.add(0, input);
 	}
 }
 
--- src/test/java/com/oocode/ExampleTest.java
+++ src/test/java/com/oocode/ExampleTest.java
@@ -1,11 +1,31 @@
 package com.oocode;
 
 import org.junit.*;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ExampleTest {
 	@Test
 	public void isEmptyInitilialised() {
-		assertTrue(new RecentlyUsedList().answer().isEmpty());
+		assertTrue(new RecentlyUsedList().getRecentlyUsedList().isEmpty());
 	}
+
+	@Test
+	public void canAddItems() {
+		final RecentlyUsedList recentlyUsedList = new RecentlyUsedList();
+		recentlyUsedList.add("ABC");
+		assertEquals(recentlyUsedList.getRecentlyUsedList(), List.of("ABC"));
+	}
+
+	@Test
+	public void firstItemInList() {
+		final RecentlyUsedList recentlyUsedList = new RecentlyUsedList();
+		recentlyUsedList.add("ABC");
+		recentlyUsedList.add("DEF");
+		assertEquals(recentlyUsedList.getRecentlyUsedList().get(0), "DEF");
+	}
+
 }
