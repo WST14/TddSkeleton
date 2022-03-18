7f8e06b41d0d8909457ea1ad07e825cda14e6b24
WST14 <wiebke.staffeldt@gmail.com>
2022-03-18 10:29:08 +0000

    create base roman numerals

--- src/main/java/com/oocode/RomanNumeralGenerator.java
+++ src/main/java/com/oocode/RomanNumeralGenerator.java
@@ -11,6 +11,16 @@ public class RomanNumeralGenerator {
             return "I";
         } else if (i == 5) {
             return "V";
+        } else if (i == 10) {
+            return "X";
+        } else if (i == 50) {
+            return "L";
+        } else if (i == 100) {
+            return "C";
+        } else if (i == 500) {
+            return "D";
+        } else if (i == 1000) {
+            return "M";
         } else {
             return "Not supported";
         }
--- src/test/java/com/oocode/RomanNumeralGeneratorTest.java
+++ src/test/java/com/oocode/RomanNumeralGeneratorTest.java
@@ -10,13 +10,17 @@ public class RomanNumeralGeneratorTest {
 	public void init() {
 		romanNumeralGenerator = new RomanNumeralGenerator();
 	}
-	@Test
-	public void testNumber1() {
-		Assert.assertEquals("I", romanNumeralGenerator.get(1));
-	}
 
 	@Test
-	public void testNumber5() {
+	public void testBaseSymbols() {
+		Assert.assertEquals("I", romanNumeralGenerator.get(1));
 		Assert.assertEquals("V", romanNumeralGenerator.get(5));
+		Assert.assertEquals("X", romanNumeralGenerator.get(10));
+		Assert.assertEquals("L", romanNumeralGenerator.get(50));
+		Assert.assertEquals("C", romanNumeralGenerator.get(100));
+		Assert.assertEquals("D", romanNumeralGenerator.get(500));
+		Assert.assertEquals("M", romanNumeralGenerator.get(1000));
 	}
+
+
 }
